f36e49f956b9aa92a79a649d877dab007a413b89
Reanz Arthur <reqbahrf@gmail.com>
2024-06-20 22:16:09 +0800

    build: :lipstick: appearance and burger menu
    improve cards appearance for the project section and add burger menu for mobile view

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('burger-menu').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    if (navLinks.style.display === 'flex') {
+      navLinks.style.display = 'none';
+    } else {
+      navLinks.style.display = 'flex';
+    }
+  });
+});
