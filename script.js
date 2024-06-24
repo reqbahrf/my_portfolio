62051ca046a73770fc1f6788dd9c5e5bf43c9f83
Reanz Arthur <reqbahrf@gmail.com>
2024-06-24 23:31:15 +0800

    refactor: :recycle: minor adjustment on the slide show javascript logic

--- script.js
+++ script.js
@@ -1,54 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const container = document.querySelector('.imageCont');
-  const images = document.querySelectorAll('.itemImg');
 
-  let maxHeight = 0;
-
-  images.forEach((image) => {
-    const height = image.offsetHeight;
-
-    if (height > maxHeight) {
-      maxHeight = height;
-    }
-  });
-
-  container.style.height = `${maxHeight}px`;
-});
 
 document.addEventListener('DOMContentLoaded', () => {
   const images = document.querySelectorAll('#prototypeImg .itemImg');
 
-  // Named function for handling scroll with a specific rate
   let handleScrollWithRate = () => handleScroll(7000);
 
   function handleScroll(rate) {
     const scrollLeft = window.scrollY;
 
     images.forEach((image, index) => {
-      const factor = (index * window.innerWidth) / rate; // Adjust this value to change the rate of movement
+      const factor = (index * window.innerWidth) / rate;
       image.style.transform = `translateX(${scrollLeft * factor}px)`;
     });
   }
 
   function checkScreenSize() {
-    if (window.innerWidth > 768) { // Adjust the width threshold as needed
-      // Use the named function for adding the event listener
+    if (window.innerWidth > 768) {
       window.addEventListener('scroll', handleScrollWithRate);
     } else {
-      // Use the same named function reference for removing
       window.removeEventListener('scroll', handleScrollWithRate);
-      // Reset handleScrollWithRate for a different condition if necessary
       handleScrollWithRate = () => handleScroll(0);
     }
   }
 
-  // Initial check
   checkScreenSize();
-
-  // Check screen size on resize
   window.addEventListener('resize', checkScreenSize);
 });
-
 
 document.addEventListener('DOMContentLoaded', () => {
   const images = document.querySelectorAll('#slideshow .itemImg');
@@ -79,10 +56,8 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('nextBtn').addEventListener('click', nextImage);
   document.getElementById('prevBtn').addEventListener('click', prevImage);
 
-  // Automatically switch images every 3 seconds
   setInterval(nextImage, 3000);
 
-  // Show the first image initially
   showImage(currentIndex);
 });
 
