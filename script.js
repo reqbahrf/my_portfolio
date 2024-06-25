232d788b3aa7c21a61e10043f8e799a15a7f8e84
Reanz Arthur <reqbahrf@gmail.com>
2024-06-26 01:03:23 +0800

    refactor: :construction: project section modals

--- script.js
+++ script.js
@@ -8,4 +8,34 @@ document.addEventListener('DOMContentLoaded', function() {
       navLinks.style.display = 'flex';
     }
   });
+
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('viewJavaProject').addEventListener('click', function(event) {
+    const modal = document.getElementById('javaProjectModal');
+    const button = event.currentTarget;
+
+    // Calculate button's position
+    const buttonRect = button.getBoundingClientRect();
+    const buttonCenterX = buttonRect.left + buttonRect.width / 2;
+    const buttonCenterY = buttonRect.top + buttonRect.height / 2;
+
+    // Set transform-origin to button's center
+    modal.style.transformOrigin = `${buttonCenterX}px ${buttonCenterY}px`;
+
+    // Toggle modal visibility and body scroll
+    modal.classList.toggle('show-modal');
+    document.body.classList.toggle('no-scroll');
+    
+    // Close modal when the close button is clicked
+    document.querySelector('.close-button').addEventListener('click', function() {
+      modal.classList.remove('show-modal');
+      document.body.classList.remove('no-scroll');
+    });
+  });
+});
+
+
+
+
